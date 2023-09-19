216fc74376bbc2d080b8a94166c9de90a344c287
Ochiengsteven <stevenochieng6305@gmail.com>
2023-09-19 13:02:40 +0300

    ADD QUERIES AND TRANSACTIONS

--- data.sql
+++ data.sql
@@ -1,7 +1,10 @@
--- Insert data into the animals table
-INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+-- Insert new data into the animals table
+INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg, species)
 VALUES
-    ('Agumon', '2020-02-03', 0, TRUE, 10.23),
-    ('Gabumon', '2018-11-15', 2, TRUE, 8.00),
-    ('Pikachu', '2021-01-07', 1, FALSE, 15.04),
-    ('Devimon', '2017-05-12', 5, TRUE, 11.00);
+    ('Charmander', '2020-02-08', 0, FALSE, -11.00, 'unspecified'),
+    ('Plantmon', '2021-11-15', 2, TRUE, -5.7, 'unspecified'),
+    ('Squirtle', '1993-04-02', 3, FALSE, -12.13, 'unspecified'),
+    ('Angemon', '2005-06-12', 1, TRUE, -45.00, 'unspecified'),
+    ('Boarmon', '2005-06-07', 7, TRUE, 20.4, 'unspecified'),
+    ('Blossom', '1998-10-13', 3, TRUE, 17.00, 'unspecified'),
+    ('Ditto', '2022-05-14', 4, TRUE, 22.00, 'unspecified');
